0ab851df4a568202361df31eccdcf4b58934212d
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2018-12-04 23:16:50 +0530

    feature: in progress: email varify

--- src/app/registration/signup/signup.component.ts
+++ src/app/registration/signup/signup.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+    constructor(private router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    navigate() {
+        this.router.navigate(['email-verify']);
+    }
 
 }
