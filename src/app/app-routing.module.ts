f8747ad358e95a6cd4a1c74f29e53b5e258c9794
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2018-12-05 11:35:16 +0530

    feature: email varify: in progress 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
-  // { path: '', loadChildren: './registration/registration-module#RegistrationPageModule' }
+  // { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './registration/registration-module#RegistrationPageModule' },
+  { path: 'email-verify', loadChildren: './email-verify/email-verify.module#EmailVerifyPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
