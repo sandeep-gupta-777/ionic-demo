5d5bdb6df53e87a17d552a9e483a732cb69611fc
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2018-12-11 12:40:08 +0530

    cordova changes

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+export enum EOnboardingStage {
+    Email,
+    Mobile,
+    FaceId
+}
+
+const ONBOARDING_LOGOS = [
+    'email@3x.png',
+    'sms-icon@3x.png'
+];
 
 @Component({
-  selector: 'app-onboarding',
-  templateUrl: './onboarding.page.html',
-  styleUrls: ['./onboarding.page.scss'],
+    selector: 'app-onboarding',
+    templateUrl: './onboarding.page.html',
+    styleUrls: ['./onboarding.page.scss'],
 })
 export class OnboardingPage implements OnInit {
 
-  constructor() { }
+    myONBOARDING_LOGOS =  ONBOARDING_LOGOS;
+    ONBOARDING_TITLE = [
+      'Email verification',
+      'Verify your mobile number',
+      'Face ID enrollment',
+    ];
+    myEOnboardingStage = EOnboardingStage;
+    stage: EOnboardingStage = this.myEOnboardingStage.FaceId;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    test(data) {
+        // alert();
+        console.log(data);
+        this.stage = data;
+    }
 
 }
